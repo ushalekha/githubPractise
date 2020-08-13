7890ff02caa2e4e608b8f11c4a49f21d4741ea62
chaitali pandya <chaitalipandya17@gmail.com>
2020-08-13 09:35:52 +0000

    Stash

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,7 +10,8 @@ sap.ui.define([
 		onClick: function () {
 			sap.m.MessageToast.show("Hi");
 			sap.m.MessageToast.show("Hi from Chaitali");
-				sap.m.MessageToast.show("Hi from ushaa");
+			sap.m.MessageToast.show("Hi from ushaa");
+			sap.m.MessageToast.show("Stash Doing");
 		}
 	});
 });
